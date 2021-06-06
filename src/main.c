ff1688ab38cdf401f5317d46ce3ea526e8083ef6
Mariano Dupont <mariano@mipscom.com.ar>
2021-06-06 11:24:20 -0300

    gpl

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+/*
+ This project is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Deviation is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Deviation.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/flash.h>
 #include <libopencm3/stm32/gpio.h>
@@ -7,6 +22,9 @@
 #include <string.h>
 #include "serial_helper.h"
 
+/*
+    Code based on https://github.com/flabbergast/libopencm3-ex/tree/master/i2c_eep
+*/
 
 // /// @brief  Possible STM32 system reset causes
 // typedef enum reset_cause_e
