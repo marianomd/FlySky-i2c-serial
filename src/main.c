fd4968f44c36dcd3d49a429bf2baa0d4e971a1bb
Mariano Dupont <mariano@mipscom.com.ar>
2021-06-08 16:33:16 -0300

    Cleanup. Added eeprom_dump.hex

--- src/main.c
+++ src/main.c
@@ -26,117 +26,6 @@
     Code based on https://github.com/flabbergast/libopencm3-ex/tree/master/i2c_eep
 */
 
-// /// @brief  Possible STM32 system reset causes
-// typedef enum reset_cause_e
-// {
-//     RESET_CAUSE_UNKNOWN = 0,
-//     RESET_CAUSE_LOW_POWER_RESET,
-//     RESET_CAUSE_WINDOW_WATCHDOG_RESET,
-//     RESET_CAUSE_INDEPENDENT_WATCHDOG_RESET,
-//     RESET_CAUSE_SOFTWARE_RESET,
-//     RESET_CAUSE_POWER_ON_POWER_DOWN_RESET,
-//     RESET_CAUSE_EXTERNAL_RESET_PIN_RESET,
-//     RESET_CAUSE_BROWNOUT_RESET,
-// } reset_cause_t;
-
-// /// @brief      Obtain the STM32 system reset cause
-// /// @param      None
-// /// @return     The system reset cause
-// reset_cause_t reset_cause_get(void)
-// {
-//     reset_cause_t reset_cause;
-
-//     if (__HAL_RCC_GET_FLAG(RCC_FLAG_LPWRRST))
-//     {
-//         reset_cause = RESET_CAUSE_LOW_POWER_RESET;
-//     }
-//     else if (__HAL_RCC_GET_FLAG(RCC_FLAG_WWDGRST))
-//     {
-//         reset_cause = RESET_CAUSE_WINDOW_WATCHDOG_RESET;
-//     }
-//     else if (__HAL_RCC_GET_FLAG(RCC_FLAG_IWDGRST))
-//     {
-//         reset_cause = RESET_CAUSE_INDEPENDENT_WATCHDOG_RESET;
-//     }
-//     else if (__HAL_RCC_GET_FLAG(RCC_FLAG_SFTRST))
-//     {
-//         reset_cause = RESET_CAUSE_SOFTWARE_RESET; // This reset is induced by calling the ARM CMSIS `NVIC_SystemReset()` function!
-//     }
-//     else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PORRST))
-//     {
-//         reset_cause = RESET_CAUSE_POWER_ON_POWER_DOWN_RESET;
-//     }
-//     else if (__HAL_RCC_GET_FLAG(RCC_FLAG_PINRST))
-//     {
-//         reset_cause = RESET_CAUSE_EXTERNAL_RESET_PIN_RESET;
-//     }
-//     // Needs to come *after* checking the `RCC_FLAG_PORRST` flag in order to ensure first that the reset cause is 
-//     // NOT a POR/PDR reset. See note below. 
-//     else if (__HAL_RCC_GET_FLAG(RCC_FLAG_BORRST))
-//     {
-//         reset_cause = RESET_CAUSE_BROWNOUT_RESET;
-//     }
-//     else
-//     {
-//         reset_cause = RESET_CAUSE_UNKNOWN;
-//     }
-
-//     // Clear all the reset flags or else they will remain set during future resets until system power is fully removed.
-//     __HAL_RCC_CLEAR_RESET_FLAGS();
-
-//     return reset_cause; 
-// }
-
-// // Note: any of the STM32 Hardware Abstraction Layer (HAL) Reset and Clock Controller (RCC) header
-// // files, such as "STM32Cube_FW_F7_V1.12.0/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h",
-// // "STM32Cube_FW_F2_V1.7.0/Drivers/STM32F2xx_HAL_Driver/Inc/stm32f2xx_hal_rcc.h", etc., indicate that the 
-// // brownout flag, `RCC_FLAG_BORRST`, will be set in the event of a "POR/PDR or BOR reset". This means that a 
-// // Power-On Reset (POR), Power-Down Reset (PDR), OR Brownout Reset (BOR) will trip this flag. See the 
-// // doxygen just above their definition for the `__HAL_RCC_GET_FLAG()` macro to see this:
-// // "@arg RCC_FLAG_BORRST: POR/PDR or BOR reset." <== indicates the Brownout Reset flag will *also* be set in 
-// // the event of a POR/PDR. 
-// // Therefore, you must check the Brownout Reset flag, `RCC_FLAG_BORRST`, *after* first checking the 
-// // `RCC_FLAG_PORRST` flag in order to ensure first that the reset cause is NOT a POR/PDR reset.
-
-
-// /// @brief      Obtain the system reset cause as an ASCII-printable name string from a reset cause type
-// /// @param[in]  reset_cause     The previously-obtained system reset cause
-// /// @return     A null-terminated ASCII name string describing the system reset cause
-// const char * reset_cause_get_name(reset_cause_t reset_cause)
-// {
-//     const char * reset_cause_name = "TBD";
-
-//     switch (reset_cause)
-//     {
-//         case RESET_CAUSE_UNKNOWN:
-//             reset_cause_name = "UNKNOWN";
-//             break;
-//         case RESET_CAUSE_LOW_POWER_RESET:
-//             reset_cause_name = "LOW_POWER_RESET";
-//             break;
-//         case RESET_CAUSE_WINDOW_WATCHDOG_RESET:
-//             reset_cause_name = "WINDOW_WATCHDOG_RESET";
-//             break;
-//         case RESET_CAUSE_INDEPENDENT_WATCHDOG_RESET:
-//             reset_cause_name = "INDEPENDENT_WATCHDOG_RESET";
-//             break;
-//         case RESET_CAUSE_SOFTWARE_RESET:
-//             reset_cause_name = "SOFTWARE_RESET";
-//             break;
-//         case RESET_CAUSE_POWER_ON_POWER_DOWN_RESET:
-//             reset_cause_name = "POWER-ON_RESET (POR) / POWER-DOWN_RESET (PDR)";
-//             break;
-//         case RESET_CAUSE_EXTERNAL_RESET_PIN_RESET:
-//             reset_cause_name = "EXTERNAL_RESET_PIN_RESET";
-//             break;
-//         case RESET_CAUSE_BROWNOUT_RESET:
-//             reset_cause_name = "BROWNOUT_RESET (BOR)";
-//             break;
-//     }
-
-//     return reset_cause_name;
-// }
-
 static void usart_setup(void)
 {
 	/* Enable clocks for GPIO port A (for GPIO_USART1_TX) and USART1. */
@@ -164,7 +53,7 @@ static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOF);
 
-	/* Set PA15 for LED */
+	/* Set PF3 for LED */
 	gpio_mode_setup(GPIOF, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO3);
 }
 
@@ -211,7 +100,6 @@ static void read_block(uint16_t addr)
 	wb[0] = (uint8_t)(addr >> 8);
 	wb[1] = (uint8_t)(addr & 0xFF);
 
-	// inicializamos a cero
 	memset(temp, 0, BLOCK_SIZE);
 
 	// read
@@ -238,7 +126,7 @@ int main(void)
 
 	i2c_setup();
 
-
+	// write example
 	// temp[0] = 0;
 	// temp[1] = 0;
 	// bytes to write
@@ -257,6 +145,7 @@ int main(void)
 	// while(t-- > 0)
 	// 	__asm__("nop");
 
+	// read example
 	// two byte addresses for bigger eeproms (change also transfer7 call!)
 	uint16_t block_num = 0;
 
@@ -272,5 +161,3 @@ int main(void)
 
 	return 0;
 }
-
-// vim: tabstop=4:shiftwidth=4:noexpandtab
